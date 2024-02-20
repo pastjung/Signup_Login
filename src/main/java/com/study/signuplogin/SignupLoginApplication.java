ddcf4d100addf1be42f5da299df975aebf829da7
정재현 <chwogus0303@naver.com>
2024-02-20 14:49:41 +0900

    init add : dependencies

---
+++ src/main/java/com/study/signuplogin/SignupLoginApplication.java
@@ -0,0 +1,13 @@
+package com.study.signuplogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignupLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/signuplogin/SignupLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.signuplogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
